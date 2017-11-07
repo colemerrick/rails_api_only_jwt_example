c6333e50e268834f3a22da4ad61c59bfa1518dc9
Cole Merrick <colegmerrick@gmail.com>
2017-11-06 18:47:46 -0800

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   post 'user_token' => 'user_token#create'
+  get 'unauthorized_response' => 'users#unauthorized_response'
+  get 'four_o_one' => 'users#four_o_one'
   get 'get_something' => 'users#get_something'
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
